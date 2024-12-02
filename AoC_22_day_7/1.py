2dcc46765840a384fc4bfbffe480a973b3f18eea
Siddharth <siddu2003oct@gmail.com>
2024-12-02 22:28:40 +0530

    only day 7

---
+++ AoC_22_day_7/1.py
@@ -0,0 +1,149 @@
+with open ('inp.txt', 'r') as file:
+    inp = file.read()
+    # print(inp)
+
+inp = inp.split("\n")
+# print(inp)
+
+dir_sizes={'main':0}
+dir_level = ['main']
+
+holy_fuck = []
+holy_fuckk = []
+
+def updater(dir_sizes, dir_level, file_size):
+    curr_lvl = "-".join(dir_level)
+    to_be_updated = ["-".join(dir_level[:i]) for i in range(1, len(dir_level)+1)]
+    # print(to_be_updated)
+    print(file_size)
+    for i in to_be_updated:
+        # print(i)
+        
+        if i in dir_sizes.keys():
+            dir_sizes[i]+=file_size
+        else:
+            dir_sizes[i] = file_size
+    
+
+# print(updater({}, ["main", 'weef', 'vijo', 'ijw'], 200))
+
+def parser(inp, start=0, end = len(inp)):
+    cnd = 0
+    dirs = []
+    for i in range(start, end):
+        if inp[i][0] == "$":
+            # print("Comm: ", inp[i])
+            if inp[i][-2:] == ' /':
+                print("main")
+                dir_level = ['main']
+            elif inp[i][-2:] == 'ls':
+                # start_ = i+1
+                # for ii in range(i+1, len(inp)):
+                #     if inp[ii][1] == "$":
+                #         end_ =ii-1
+                #         break
+                # parser(inp, start_ = start, end = end_)
+                # print("oru ls")
+                print("", end='')
+
+            elif inp[i] == '$ cd ..':
+                # print("oru lvl veliya")
+                dir_level.pop()
+            
+            elif inp[i][:4] == '$ cd' and inp[i]!= "$ cd ..":
+                # print("oru lvl ulla")
+                names = inp[i].split(" ")
+                # print(names, len(names))
+                dir_level.append(names[-1])
+
+        elif inp[i][:3] == 'dir':
+            cnd+=1
+            names = inp[i].split(" ")
+            # try:
+            # out = dir_sizes[names[-1]]
+                # print(out, names[-1])
+            # except:
+            l_dir = "-".join(dir_level)+"-"+names[-1]
+            if l_dir in dir_sizes.keys():
+                print('irukku', dir_level, names[-1])
+            else:
+                dir_sizes[l_dir]=0
+            dirs.append("-".join(dir_level) +"-"+names[-1])
+        
+        else:
+            names = inp[i].split(" ")
+            
+            # print(names, len(names))
+            # for lvl in range(0, -len(dir_level), -1):
+
+                # dir_sizes[dir_level[lvl]]+= int(names[0])
+            updater(dir_sizes, dir_level, int(names[0]))
+                # curr -= 1
+
+    print("cnd =", cnd)
+#     print(dir_level)
+parser(inp)
+# print(dir_sizes)
+
+tot_sum = 0
+match_dir = []
+match_nums = []
+
+for i in dir_sizes.keys():
+    if dir_sizes[i] <= 100000:
+        # print(dir_sizes[i])
+        tot_sum+=dir_sizes[i]
+        match_dir.append((i, dir_sizes[i]))
+        match_nums.append(dir_sizes[i])
+    # print(i, dir_sizes[i])
+print(tot_sum)
+print(len(match_dir))
+
+# for i in match_dir:
+#     print(i)
+
+print(len(dir_sizes.keys()))
+# print(dir_sizes['nfz'], dir_sizes['jtzw'] )
+
+n = 0
+
+for i in dir_sizes.values():
+    if i <= 100000:
+        n+=i
+#         print(n)
+# print(n)
+
+
+
+
+
+
+# for i in dir_sizes.keys():
+#     print(i,"  ", dir_sizes[i])
+
+
+
+
+
+occ_dir = []
+
+# for i in inp:
+#     if i[:3] == 'dir':
+#         names = i.split(" ")
+#         occ_dir.append(names[-1])
+
+# # occ_dir = set(occ_dir)
+# print(len(list(set(occ_dir))))
+# print(occ_dir)
+# print(len(occ_dir))
+
+# new_effed_sum = 0
+# for i in occ_dir:
+#     if dir_sizes[i]<=100000:
+#         new_effed_sum+=dir_sizes[i]
+
+# print(new_effed_sum)
+
+# 1075948
+
+# main = 40208860
---
+++ AoC_22_day_7/k.py
@@ -0,0 +1,136 @@
+
+# ----------------------------------------------- libraries ------------------------------------------------- #
+
+import rich
+
+# ----------------------------------------------- constants ------------------------------------------------- #
+
+# Given puzzle contains some constants like,
+# ```
+# maximum size to be considered small = MAX_SIZE
+# total size of the disk              = TOTAL_SIZE
+# required size for the update        = REQ_SIZE
+# ```
+
+MAX_SIZE = 100000
+TOTAL_SIZE = 70000000
+REQ_SIZE = 30000000
+
+# ----------------------------------------------- get input ------------------------------------------------ # 
+
+with open("inp.txt", "r") as file:
+    raw_data = [line.strip() for line in file.readlines()]
+    file.close()
+
+# ----------------------------------------------- directree ------------------------------------------------ # 
+
+# storing the directories and their sub_dirs/files in a dictionary
+
+tree = {
+    '/' : []
+}
+
+# ---------------------------------------------- parse input ----------------------------------------------- # 
+
+# current directory path
+curr = '/'
+
+for line in raw_data:
+    cmds = line.split()
+    if cmds[0] == '$':
+        match cmds[1]:
+            case 'cd':
+                if cmds[2] == '/':
+                    curr = '/'
+
+                # To go back one diretory, remove the last part after the '-'
+                # in the current directory value.
+                # if curr = '/-old_dir-new_dir', after changing, it becomes,
+                # curr = '/-old_dir'
+                elif cmds[2] == '..':
+                    curr = '-'.join(curr.split('-')[:-1])
+
+                # If we 'cd' into a new directory, the current directory becomes
+                # 'curr + new_dir'. This is to avoid overriding of values in the 'tree' 
+                # dictionary since there are duplicate values in the directory structure.
+                else:
+                    curr = curr + '-' + cmds[2]
+
+            case 'ls':
+                pass
+    else:
+        # if the first value is numeric, it's a file
+        if cmds[0].isnumeric():
+            tree[curr].append((cmds[1], int(cmds[0])))
+
+        # else it's a folder
+        elif cmds[0] == "dir":
+            tree[curr + '-' + cmds[1]] = []
+            tree[curr].append(curr + '-' + cmds[1])
+
+# -------------------------------------- calculate directory sizes ----------------------------------------- # 
+
+# dictionary to keep values of all the directories in the tree
+size = {}
+
+def get_size(dir):
+    total = 0
+    for sub_dir in tree[dir]:
+        if type(sub_dir) != tuple:
+
+            # recursively check all the sub directories
+            total += get_size(sub_dir)
+        else:
+            total += sub_dir[1]
+
+    # store the space of the directory in the size dict
+    size[dir] = total
+    return total
+
+# ------------------------------------ print tree function for debugging ------------------------------------ # 
+
+def printtree(dir, n = 0):
+    if size[dir] > MAX_SIZE:
+        rich.print('    ' * n, f"[bold red]{dir}[/bold red]")
+    else:
+        rich.print('    ' * n, f"[bold cyan]{dir}[/bold cyan]")
+        
+    for sub_dir in tree[dir]:
+        if type(sub_dir) != tuple:
+            printtree(sub_dir, n + 1)
+        else:
+            print('    ' * (n + 1), '-- ', sub_dir)
+
+# ------------------------------------------------- part 1 -------------------------------------------------- #
+
+solution_1 = 0
+disk_size = get_size('/')
+
+for dir, space in size.items():
+    if space <= 100000:
+        solution_1 += space
+
+print(len(size.keys()))
+# print(len(list(set(size.keys()))))
+
+# ------------------------------------------------ part 2 --------------------------------------------------- #
+
+unused_space = TOTAL_SIZE - disk_size
+unused_dirs = []
+
+for dir, space in size.items():
+    if unused_space + space >= 30000000:
+        unused_dirs.append(space)
+
+solution_2 = min(unused_dirs)
+
+# ----------------------------------------------- solution -------------------------------------------------- #
+
+print(f"Part 1: {solution_1}")
+print(f"Part 2: {solution_2}")
+
+# ----------------------------------------------------------------------------------------------------------- #\\
+
+
+for i in size.keys():
+    print(i, " ", size[i])
